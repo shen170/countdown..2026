b70a06cd0c00623b026aef6f024afc4a63051a3b
shen170 <shehanjayatissa@gmail.com>
2025-12-12 11:42:47 +0530

    Add files via upload

---
+++ Countdown.js
@@ -0,0 +1,22 @@
+function updateCountdown() {
+    const newYear = new Date("January 1, 2026 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = newYear - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const d = Math.floor(gap / day);
+    const h = Math.floor((gap % day) / hour);
+    const m = Math.floor((gap % hour) / minute);
+    const s = Math.floor((gap % minute) / second);
+
+    document.getElementById("days").innerText = d;
+    document.getElementById("hours").innerText = h;
+    document.getElementById("minutes").innerText = m;
+    document.getElementById("seconds").innerText = s;
+}
+
+setInterval(updateCountdown, 1000);
